8b6298582698631c9a7a73ad1758e42cbbb58cd0
leonardoyusuke <leoyuhirano@gmail.com>
2023-02-02 17:47:25 +0900

    post choice

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import { MongoClient } from "mongodb";
 import dayjs from "dayjs";
 import Joi from "joi";
+import { ObjectId } from "mongodb";
 
 
 const server = express()
@@ -41,7 +42,7 @@ server.post ("/poll", async (req,res)=>{
     if(expireAt === null ){
         const currentDate = dayjs();
         const futureDate = currentDate.add(30, 'day');
-        expireAt = futureDate.format("DD/MM/YYYY")
+        expireAt = futureDate.format("DD/MM/YYYY HH:MM")
     }
     const postPoll = {
         title:title,
@@ -69,6 +70,64 @@ server.get("/poll", async (req,res)=>{
 
 })
 
+server.post("/choice", async(req,res)=>{
+    const {title,pollId} = req.body
+    const escolha = {
+        title:title,
+        pollId:pollId}    
+    try {
+        const validacaoId = await db.collection("poll").find({_id:pollId})
+        const validacaoTitle = await db.collection("choice").find({pollId , title})
+       console.log(validacaoTitle,"titulo")
+        if(!validacaoId){
+                res.sendStatus(404)
+                return
+            }
+       if(validacaoTitle){
+       res.sendStatus(409)
+        return
+    }
+        const teste =  await db.collection("choice").insertOne(escolha)
+        res.sendStatus(201)
+        console.log(teste,"teste")
+
+    } catch (error) {
+        res.send(error)
+        
+    }
+} )
+
+server.get("/poll/:id/vote", async (req,res)=>{
+    const pollId = req.params.id
+    console.log(pollId)
+try {
+    console.log("entrou")
+    const validacao = await db.collection("choice").findOne({pollId})
+    console.log(validacao)
+    if(!validacao){
+       return res.sendStatus(404)
+    }
+    const choices = await db.collection("choice").find({pollId}).toArray()
+    return res.send(choices)
+} catch (error) {
+    return res.send(error)
+    
+}
+})
+
+server.post("/choice/:id/vote", async(req,res)=>{
+    const pollId = req.params
+    try {
+        await db.collection("choice").insertOne({      createdAt: dayjs().format("YYYY-MM-DD HH:mm"),
+        choiceId: pollId})
+        res.sendStatus(201)
+    } catch (error) {
+        res.sendStatus(500)
+        
+    }
+
+
+})
 
 server.listen(PORT, () => {
     console.log(`server on port ${PORT}`)
