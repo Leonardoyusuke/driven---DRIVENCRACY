8750edc3899d70b1b9fbea44e862a8116497b73c
leonardoyusuke <leoyuhirano@gmail.com>
2023-02-03 22:02:49 +0900

    asd

--- src/Controllers/pollPost.js
+++ src/Controllers/pollPost.js
@@ -10,8 +10,8 @@ export default async (req,res)=>{
     console.log(req.body)
     
     if (expireAt == null) {
-        const noExpireAt = dayjs().format("YYYY-MM-DD HH:mm")
-        expireAt = noExpireAt.add(30, "day")
+        const noExpireAt = dayjs();
+        expireAt = noExpireAt.add(30, "day").format("YYYY-MM-DD HH:mm");
         console.log(expireAt)
       } else {
         expireAt = dayjs(expireAt).format("YYYY-MM-DD HH:mm");
